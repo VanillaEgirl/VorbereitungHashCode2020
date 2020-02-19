7b905c079b7ade18105d9c09bbc9ae5ea39fa099
Thomas Tieber <Thomas.Tieber@catalysts.cc>
2020-02-19 19:57:19 +0100

    remove useless sout

--- src/root/Main.java
+++ src/root/Main.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo do Schneckli!");
-
         List<String> strings = FileReader.readFile();
         FileWriter.writeFile(strings);
     }
